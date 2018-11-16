b7f6b27a3ba876e0db0014735430c5df0b6eef5c
Luan Messias da Silva <luan.silva@rakuten.com.br>
2018-11-16 16:48:56 -0300

    Deploy

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -6,10 +6,9 @@ function initializeClock() {
     var month = dateObj.getUTCMonth() + 1; //months from 1-12
     var day = dateObj.getUTCDate() + 1;
     var year = dateObj.getUTCFullYear();
-    console.log(day);
 
     // Set the date we're counting down to
-    var countDownDate = new Date("Nov "+ day +", 2018 00:00:00").getTime();
+    var countDownDate = new Date("Nov " + day + ", 2018 00:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function () {
@@ -49,18 +48,96 @@ $(document).ready(function () {
 
     initializeClock();
 
-    $('.header__mobsearch').click(function(){
+    $('.header__mobsearch').click(function () {
         $('.header__mobsearch__bt').toggleClass('active');
         $('.header__search').toggleClass('active');
     });
 
-    $('.cs_mobmenu').click(function(){
+    $('.cs_mobmenu').click(function () {
         $(this).toggleClass('active');
         $('.cs_menu').toggleClass('active');
     });
 
 
+    //Thumb
+    var thumbsTotal = $(".cs_prod__thumb__item").length;
+    var thumbsArrows = 1;
+    var thumbsMax = $(".cs_prod__thumb__item").length - 4;
+    var thumbsMin = 1;
+
+    $('#arrowUp').click(function () {
+        if(thumbsMin != 1){
+            $('.cs_prod__thumb__cont').animate({'top': '+=120px'},'slow');
+            $(this).removeClass('inactive');
+            $('#arrowDown').removeClass('inactive');
+            thumbsMin--
+        } else {
+            $(this).addClass('inactive');
+        }
+    });
+
+
+    $('#arrowDown').click(function () {
+
+        if(thumbsMin <= thumbsMax){
+            $('.cs_prod__thumb__cont').animate({'top': '-=120px'},'slow');
+            $(this).removeClass('inactive');
+            $('#arrowUp').removeClass('inactive');
+            thumbsMin++
+        } else {
+            $(this).addClass('inactive');
+        }
+    });
+
+    $('.cs_prod__thumb__item__img').click(function(){
+        var url = $(this).attr('src');
+        var url = url.replace('thumb/', '');
+        $('.cs_prod__thumb__item').removeClass('active');
+        $(this).parent().addClass('active');
+        $('.cs_prod__photo__img').attr('src', url);
+    });
+
+    
+    $('#arrowLeft').click(function () {
+        
+        if(thumbsArrows >= 1){
+            if (thumbsArrows > 1){
+                thumbsArrows--
+            }
+            $('.cs_prod__photo__img').attr('src', 'dist/img/prod1/' + thumbsArrows + '.jpg');
+            $('.cs_prod__nav__arrow').removeClass('inactive');
+        }
+
+        if (thumbsArrows == 1){
+            $(this).addClass('inactive');
+        }
+
+    });
+
+    $('#arrowRight').click(function () {
+        
+        if(thumbsArrows <= thumbsTotal){
+            if (thumbsArrows < thumbsTotal){
+                thumbsArrows++
+            }
+            $('.cs_prod__photo__img').attr('src', 'dist/img/prod1/' + thumbsArrows + '.jpg');
+            $('.cs_prod__nav__arrow').removeClass('inactive');
+        }
+        if (thumbsArrows == thumbsTotal){
+            $(this).addClass('inactive');
+        }
+    });
+
+    $('.cs_prod__nav__arrow.active').click(function(){
+        eventPreventDefault();
+    });
+
 });
+
+
+
+
+
 
 
 
